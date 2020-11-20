5d68bd038c6aab58600d14a1273c4b282db29e1f
Semi Lee <semilee6557@gmail.com>
2020-11-20 05:24:57 +0000

    Add a PageHeader Component

---
+++ components/page-header.js
@@ -0,0 +1,9 @@
+class PageHeader {
+  constructor(headerElement, pageHeader) {
+    this.headerElement = headerElement;
+    this.pageHeader = pageHeader;
+  }
+  updateAverage(newAverage) {
+    console.log(newAverage)
+  }
+}
